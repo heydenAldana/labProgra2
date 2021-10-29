3938ec71511cbc6e64b312696c56a1233a01280f
heydenAldana <heydenalfonsoaldana@gmail.com>
2021-10-29 13:41:40 -0600

    primer comentario

---
+++ src/lab3_progra2_heydenaldana22111098/Lab3_Progra2_HeydenAldana22111098.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab3_progra2_heydenaldana22111098;
+
+/**
+ *
+ * @author heyde
+ */
+public class Lab3_Progra2_HeydenAldana22111098 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
